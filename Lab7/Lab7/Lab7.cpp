0b08905b6657adc0a85861c2838284688b38c74a
yinwanng <xxjohnnyng@gmail.com>
2018-03-21 13:03:01 -0700

    Added another test in main

--- Lab7/Lab7/Lab7.cpp
+++ Lab7/Lab7/Lab7.cpp
@@ -30,6 +30,30 @@ int main()
 	cout << "\nThe average value in the vector is: " << average(values);
 
 	cout << endl << endl;
+
+
+	vector<int> values2{ 5 };
+
+	// print vector content
+	cout << "The numbers in the vector are: ";
+	for (int i = 0; i < values2.size(); i++) {
+		cout << values[i] << " ";
+	}
+
+	// print sum of the values in the vector
+	cout << "\nThe sum of the vector is: " << sum(values2);
+
+	// print the max value in the vector
+	cout << "\nThe max value in the vector is: " << max(values2);
+
+	// print the min value in the vector
+	cout << "\nThe min value in the vector is: " << min(values2);
+
+	// print the average value in the vector
+	cout << "\nThe average value in the vector is: " << average(values2);
+
+	cout << endl << endl;
+
 	system("pause");
 	return 0;
 }
